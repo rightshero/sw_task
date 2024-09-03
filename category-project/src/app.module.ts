12d83b46203023b2a520d7f6f7fc5777571ae5c4
Zeyad Tarek <zeyad.ta01@gmail.com>
2024-09-03 12:47:10 +0300

    Create the simple front end

--- category-project/src/app.module.ts
+++ category-project/src/app.module.ts
@@ -1,9 +1,54 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoryModule } from './category/category.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Category } from './category/entities/category.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    CategoryModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'development' ? `.env.development` : '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        if (process.env.NODE_ENV === 'development') {
+          return {
+            type: 'postgres',
+            ssl: process.env.NODE_ENV === 'development' ? false : true,
+            host: config.getOrThrow<string>('DATABASE_HOST'),
+            port: config.getOrThrow<number>('DATABASE_PORT'),
+            database: config.getOrThrow<string>('DATABASE_NAME'),
+            username: config.getOrThrow<string>('DATABASE_USER'),
+            password: config.getOrThrow<string>('DATABASE_PASSWORD'),
+            entities: [Category],
+            logger: 'file',
+            logging: false,
+            synchronize: true,
+          };
+        } else {
+          return {
+            type: 'postgres',
+            ssl: process.env.NODE_ENV === 'development' ? false : true,
+            host: config.getOrThrow<string>('DATABASE_HOST'),
+            port: config.getOrThrow<number>('DATABASE_PORT'),
+            database: config.getOrThrow<string>('DATABASE_NAME'),
+            username: config.getOrThrow<string>('DATABASE_USER'),
+            password: config.getOrThrow<string>('DATABASE_PASSWORD'),
+            entities: [Category],
+            logger: 'file',
+            logging: false,
+            synchronize: true,
+          };
+        }
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
