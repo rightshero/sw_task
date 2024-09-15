7445a7454da87e8fa233c010232519ee31bebee8
FarahKhalill <147333167+FarahKhalill@users.noreply.github.com>
2024-09-15 18:13:54 +0300

    build: app completed and integrated locally

--- dynamic_checkbox/main/urls.py
+++ dynamic_checkbox/main/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns=[
     # path("", views.index, name="index"),
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path('update_checkbox/', views.update_checkbox, name='update_checkbox'),  # For AJAX
+    path('clear_checkboxes/', views.clear_checkboxes, name='clear_checkboxes'), # For AJAX-> used when page is refreshed
+
 ]
