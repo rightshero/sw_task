80dcf7224d8ed37551e459cfa5d6c400a71a0be7
FarahKhalill <147333167+FarahKhalill@users.noreply.github.com>
2024-09-16 07:19:43 +0300

    fix: npm app deletion

--- server/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  development: {
-    username: "your_username",
-    password: "your_password",
-    database: "your_database",
-    host: "127.0.0.1",
-    dialect: "postgres",
-  },
-};
