a374e527fb5cc504dec5a6cc47a3e1f2143e3899
FarahKhalill <147333167+FarahKhalill@users.noreply.github.com>
2024-09-16 04:45:56 +0300

    build: containerized app and postgres db

--- dynamic_checkbox/dynamic_checkbox/settings.py
+++ dynamic_checkbox/dynamic_checkbox/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main.apps.MainConfig'
+    'main.apps.MainConfig',
+    'dynamic_checkbox'
+    
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,14 @@ WSGI_APPLICATION = 'dynamic_checkbox.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('DB_DRIVER','django.db.backends.postgresql'),
+        'NAME': os.environ.get('POSTGRES_DB','postgres'),
+        'USER': os.environ.get('POSTGRES_USER','postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD','postgres'),
+        'HOST': os.environ.get('POSTGRES_HOST','localhost'),
+        'PORT': os.environ.get('POSTGRES_PORT','5432'),
     }
 }
 
