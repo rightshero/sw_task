6aad3e9b86aad12137820784c1ea5afdfd9941dd
yahyamohmuedpro99 <yahyamohmuedpro99@gmail.com>
2024-09-24 01:17:29 +0300

    fix: update naming from proj to project

--- dynamic-catgory-app/category_project/urls.py
+++ dynamic-catgory-app/category_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for category_proj project.
+URL configuration for category_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
