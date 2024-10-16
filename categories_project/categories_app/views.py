c5d58c13ce5ebb99ec8bffc9a2755c053bddaea2
MohanadGad38 <90938419+MohanadGad38@users.noreply.github.com>
2024-10-16 21:30:29 +0300

    Create: seeder

--- categories_project/categories_app/views.py
+++ categories_project/categories_app/views.py
@@ -6,14 +6,17 @@ from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 
-def index(request):
+def index(request) -> HttpResponse:
     all_categories = Category.objects.filter(parent__isnull=True)
     return render(request, 'index.html', {'categories': all_categories})
+
+# The CSRF middleware and template tag provides easy-to-use protection against cross-site request forgery attacks.
+# check the documentation https://docs.djangoproject.com/en/5.1/ref/csrf/
 
 
 @csrf_exempt
 @require_http_methods(["POST"])
-def create_category(request):
+def create_category(request) -> JsonResponse:
     name = request.POST.get('name')
     parent_id = request.POST.get('parent_id')
     parent = Category.objects.get(id=parent_id)
