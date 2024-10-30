ca4867ec11f1d2c9539a98a3ce6905fb28245765
usamaalzomor <usama.alzomor@shelter-technology.com>
2024-10-30 21:20:35 +0300

    feat(categories): Create the model for categories app

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    def __str__(self):
+        return self.name
