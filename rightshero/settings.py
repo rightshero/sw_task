8dfac01df527f0a93bc13cc1257bccc84ab21a50
usamaalzomor <usama.alzomor@shelter-technology.com>
2024-11-01 18:11:38 +0200

    chore(config): load SECRET_KEY and DEBUG from environment variables

--- rightshero/settings.py
+++ rightshero/settings.py
@@ -20,15 +20,11 @@ env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y2a%3e1&eaztok8%5#&ivx*e*dccw43*u04(!qrjqav0e%^a^8'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = ["0.0.0.0"]
 
