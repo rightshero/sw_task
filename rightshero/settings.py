cba34a269645e85424b107bbc573ee5c44a7d19b
usamaalzomor <usama.alzomor@shelter-technology.com>
2024-11-01 18:19:11 +0200

    fix: bad request error related to ALLOWED_HOSTS

--- rightshero/settings.py
+++ rightshero/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = ["0.0.0.0", "*"]
 
 
 # Application definition
