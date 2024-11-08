5c3243cb7abf1ba9a16633c613900a8bdfab55f2
ahmedelgamal0 <a.elgamal98@outlook.com>
2024-11-08 21:52:45 +0200

    overwrite perform update

--- categories/views.py
+++ categories/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from .models import Category
 from .serializers import CategorySerializer
+from rest_framework import serializers
 
 def index(request):
     """
     Renders the index.html template.
     """
-    return render(request, 'categories/index.html') 
+    return render(request, 'categories/index.html')
 
 class CategoryViewSet(viewsets.ModelViewSet):
 
@@ -18,17 +19,9 @@ class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
     permission_classes = [permissions.IsAuthenticated]
 
-    def perform_create(self, serializer):
-        if serializer.validate_data.get('parent') == self.request.data.get('id'):
-                raise serializer.ValidationError("Category cannot be its own parent.")
-        serializer.save()
-        return super().perform_create(serializer)
-
-
     def perform_update(self, serializer):
-        """
-        Override the update method to perform the validation before saving.
-        """
-        if serializer.validated_data.get('parent') == self.kwargs.get('pk'):
-            raise serializer.ValidationError("Category cannot be its own parent.")
+        if serializer.validated_data.get('parent') == self.request.data.get('parent'):
+                raise serializers.ValidationError("Category cannot be its own parent.")
         serializer.save()
+        return super().perform_update(serializer)
+
