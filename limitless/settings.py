b6edcae77d43b2a9ed583bdbfefd4dee66bbb76e
Ahmed Omar <ahmed42795@gmail.com>
2024-11-14 18:45:49 +0200

    improvements

--- limitless/settings.py
+++ limitless/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5m-a_g#^z-tz*pw4))fta(e&%(*m6vp++#rn+%lc486fwlk_g7'
+SECRET_KEY = os.environ['DJANGO_SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
